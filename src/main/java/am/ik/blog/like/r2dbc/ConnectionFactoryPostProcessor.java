548d30acce7b0a0451998eb8be8473249c0b18c4
Toshiaki Maki <tmaki@pivotal.io>
2020-06-09 01:36:11 +0900

    Add remote service name

--- src/main/java/am/ik/blog/like/r2dbc/ConnectionFactoryPostProcessor.java
+++ src/main/java/am/ik/blog/like/r2dbc/ConnectionFactoryPostProcessor.java
@@ -3,25 +3,26 @@ package am.ik.blog.like.r2dbc;
 import brave.Tracer;
 import io.r2dbc.proxy.ProxyConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnectionFactoryPostProcessor implements BeanPostProcessor {
-    private final Tracer tracer;
+	private final Tracer tracer;
 
-    public ConnectionFactoryPostProcessor(Tracer tracer) {
-        this.tracer = tracer;
-    }
+	public ConnectionFactoryPostProcessor(Tracer tracer) {
+		this.tracer = tracer;
+	}
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof ConnectionFactory) {
-            return ProxyConnectionFactory.builder((ConnectionFactory) bean)
-                    .listener(new TracingExecutionListener(tracer))
-                    .build();
-        }
-        return bean;
-    }
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof ConnectionFactory) {
+			return ProxyConnectionFactory.builder((ConnectionFactory) bean)
+					.listener(new TracingExecutionListener(tracer, "blog:like-db"))
+					.build();
+		}
+		return bean;
+	}
 }
